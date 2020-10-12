e2685497e394ed42faf4dae6e7672d295593a579
LmfZoozle <LmfZoozle@gmail.com>
2020-10-12 23:07:19 +0900

    read_file

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -3,17 +3,23 @@ mod parser;
 
 use std::env::args;
 use std::process::exit;
+use std::fs;
 
 const PATH: &str = "~/Code/Zle/Edition3/test";
 const IS_DEBUG:bool=true;
 
-//const NAME_ZLE:&str="test.zle";
-//const NAME_BYTECODE:&str="test.bc";
-
 fn main() {
     eprintln!("zle compiler activated.");
     let option: Vec<String> = std::env::args().collect();
-    if option.len() < 2 {}
+    if option.len() < 2 {
+        eprintln!("lack");
+        exit(1);
+    }
     println!("This is from zle compiler");
-    println!("")
+    let name=&option[0];
+    let code=if let Ok(a)=std::fs::read_to_string(name){
+        a
+    }else{
+        exit(2);
+    };
 }
