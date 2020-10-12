eaeba9f6ee621d555f8bd2099b1eb96b41f354ec
LmfZoozle <LmfZoozle@gmail.com>
2020-10-12 22:57:58 +0900

    made_shell_script

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -7,9 +7,13 @@ use std::process::exit;
 const PATH: &str = "~/Code/Zle/Edition3/test";
 const IS_DEBUG:bool=true;
 
+//const NAME_ZLE:&str="test.zle";
+//const NAME_BYTECODE:&str="test.bc";
+
 fn main() {
     eprintln!("zle compiler activated.");
     let option: Vec<String> = std::env::args().collect();
     if option.len() < 2 {}
-
+    println!("This is from zle compiler");
+    println!("")
 }
