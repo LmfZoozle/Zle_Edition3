e6df1d3ad2abbba5899da9789e5776b1b99f7307
LmfZoozle <LmfZoozle@gmail.com>
2020-10-16 23:18:00 +0900

    パースしたら後置記法に書き換える

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -6,7 +6,7 @@ mod generator;
 use std::process::exit;
 //use std::fs;
 
-const PATH: &str = "~/Code/Zle/Edition3/test";
+//const PATH: &str = "~/Code/Zle/Edition3/test";
 const IS_DEBUG:bool=true;
 
 fn main() {
