76fd7646ab270543408dfe552853cf2e027ee660
LmfZoozle <LmfZoozle@gmail.com>
2020-10-22 16:16:12 +0900

    順位解決はgenerator.rsで

--- compiler/src/parser.rs
+++ compiler/src/parser.rs
@@ -60,53 +60,6 @@ pub fn token_into_priorty(tokens: Vec<lexer::Token>) -> Vec<PriorityVal> {
     result
 }
 
-pub fn change_prival(mut from: Vec<PriorityVal>) -> Vec<PriorityVal> {
-    // 5 + 8 * 4 - 7
-    let mut count = 0;
-    while count < from.len() {
-        if *from[count].get_level() == Priority::Lev2 {
-            if count != 0 {
-                from[count - 1].set_level(Priority::Lev2);
-            }
-            if count > 1 {
-                from[count - 2].set_level(Priority::Lev2);
-            }
-            if count != from.len() - 1 {
-                from[count + 1].set_level(Priority::Lev2);
-            }
-        }
-        count += 1;
-    }
-
-    from
-}
-
-/*fn howlong_lev2(target: &Vec<PriorityVal>, idx: usize) -> usize {
-    let mut count = idx;
-    let mut result = 0;
-    while count < target.len() {
-        if *target[count].get_level() != Priority::Lev2 {
-            return result;
-        }
-        result += 1
-    }
-    return result;
-}*/
-
-pub fn sort_changed(from: &mut Vec<PriorityVal>) -> Vec<&PriorityVal> {
-    //let a = from.split(|x| *x.get_level() != Priority::Lev2);
-    let mut lv2: Vec<&PriorityVal> = from
-        .iter()
-        .filter(|x| *x.get_level() == Priority::Lev2)
-        .collect();
-    let mut notlv2: Vec<&PriorityVal> = from
-        .iter()
-        .filter(|x| *x.get_level() != Priority::Lev2)
-        .collect();
-    lv2.append(&mut notlv2);
-    lv2
-}
-
 pub struct Node {
     left: Box<Node>,
     right: Box<Node>,
