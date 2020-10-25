f86434449d5ee6ae9c436f0353c078b74136a08c
LmfZoozle <LmfZoozle@gmail.com>
2020-10-25 23:00:07 +0900

    空白ｗ

---
+++ test/test.s
@@ -0,0 +1,21 @@
+.intel_syntax noprefix
+.globl main
+main:
+push 20
+push 4
+  pop rdi
+  pop rax
+  add rax, rdi
+push rax
+push 8
+push 2
+  pop rdi
+  pop rax
+  imul rax, rdi
+push rax
+  pop rdi
+  pop rax
+  sub rax, rdi
+push rax
+  pop rax
+  ret
