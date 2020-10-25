e77a1b2a856a3083b053d06f296687e7ba698c9d
LmfZoozle <LmfZoozle@gmail.com>
2020-10-25 19:43:41 +0900

    implement

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -22,6 +22,42 @@ pub enum Token{
     Braket(Brackets),
 }
 
+pub fn as_long_as_num(input:&String)->Option<i32>{
+    //let mut chars=input.chars().collect::<Vec<char>>();
+    //流石にこのままの実装は手間かかりすぎダヨナー
+    let lim=input.len() as i32+1;
+    let mut c=0;
+    let mut temporal=0;
+    let mut done=false;
+    while c<lim{
+        let mut buff=String::new();
+        let mut inner=0;
+        for run in input.chars(){
+            buff.push(run);
+            inner+=1;
+            if inner==c{
+                break;
+            }
+        }
+        if let Ok(num) =buff.to_string().parse::<i32>(){
+            temporal=num;
+            done=true;
+        }
+        c+=1;
+    }
+
+    if done{
+        Some(temporal)        
+    }else{
+        None
+    }
+}
+
+pub fn Ex_read_into_token(input:String)->Vec<Token>{
+    let mut result=Vec::new();
+    
+    result
+}
 
 pub fn read_into_token(input:String)->Vec<Token>{
     let mut result=Vec::new();
